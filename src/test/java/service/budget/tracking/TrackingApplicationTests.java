494e130df742ab8bfd3be6cf84d80c2bfbfaf21b
Jonathan Lopez <toribioj818@gmail.com>
2024-07-29 10:53:31 -0400

    Change structure, fix some error in spec utils

--- src/test/java/service/budget/tracking/TrackingApplicationTests.java
+++ src/test/java/service/budget/tracking/TrackingApplicationTests.java
@@ -6,27 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrackingApplicationTests {
-
 	@Test()
 	void contextLoads() {
-		Assertions.assertEquals(true, true);
 	}
-
-	@Test()
-	void contextLoads2() {
-		Assertions.assertEquals(true, true);
-	}
-
-	@Test()
-	void contextLoads3() {
-		Assertions.assertEquals(true, false);
-	}
-
-	@Test()
-	void contextLoads4() {
-		Assertions.assertEquals(true, true);
-	}
-
-
-
 }
